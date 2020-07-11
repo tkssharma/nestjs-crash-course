3949642fe0a04e653fdce9f2232f9757c21726e9
tkssharma <tarun.softengg@gmail.com>
2020-07-12 00:54:41 +0530

    mongo db module updated for customer model

--- src/customer/controller/customer.controller.ts
+++ src/customer/controller/customer.controller.ts
@@ -12,8 +12,12 @@ export class CustomerController {
 
   @Get()
   async getAllCustomers(@Res() res: Response) {
+    try {
     const data = await this.service.listCustomer();
     res.status(HttpStatus.OK).json(data);
+  } catch(err){
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err);
+  }
   }
 
   @Post()
@@ -30,7 +34,6 @@ export class CustomerController {
   async getCustomerById( @Param() param: CustomerParamDTO) {
     return await this.service.getCustomer(param.customerId);
   }
-
   @Delete('/')
   async deleteCustomerById(@Query('customerid') id: string) {
     return await this.service.getCustomer(id);
